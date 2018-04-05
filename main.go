6d44a4b6ec28a3974ae437a858f72946e610c52f
Igor Chaban <lintuh@gmail.com>
2018-04-05 09:29:21 +0300

    Finished text version

--- main.go
+++ main.go
@@ -6,47 +6,49 @@ import (
     "os"
     "log"
     "bufio"
+    g "github.com/soniah/gosnmp"
+    "strconv"
+    "strings"
+    "time"
 )
 
-func main() {
-    mysqlHost := flag.String("host", "", "mysql hostname")
-    mysqlPort := flag.Int("port", 3306, "mysql port")
-    mysqlUser := flag.String("user", "", "mysql user")
-    mysqlPass := flag.String("pass", "", "mysql password")
-    fileName := flag.String("file", "", "file containing ip addresses")
-    showResults := flag.Bool("results", false, "show results")
+var ipFileName = flag.String("ip-file", "", "file containing ip addresses")
+var outFileName = flag.String("out-file", "", "file for output write")
 
+func main() {
     flag.Parse()
 
-    if len(*mysqlHost) == 0 || len(*mysqlUser) == 0 || len(*mysqlPass) == 0 || len(*fileName) == 0 || *mysqlPort == 0 {
+    if len(*ipFileName) == 0 || len(*outFileName) == 0 {
         printUsage()
     }
 
-    if *showResults == true {
-        results()
-    }
-
-    data, err := readFile(*fileName)
+    data, err := readFile(*ipFileName)
     if err != nil {
         log.Fatalf("error: %s", err.Error())
     }
 
-    fmt.Println(data)
+    if len(data) == 0 {
+        log.Fatal("error: file is empty")
+    }
+
+    for ip := range data {
+        getSnmpData(data[ip])
+    }
 }
 
 func printUsage()  {
-    fmt.Println("snmp-swith-port-monitor -- monitor port up/down state via SNMP and writing results into MySQL")
+    fmt.Println("snmp-swith-port-monitor -- monitor port up/down state via SNMP")
     fmt.Println("")
     fmt.Println("Usage:")
     flag.PrintDefaults()
     fmt.Println("")
     fmt.Println("Examples:")
-    fmt.Println("  snmp-switch-port-monitor --host host --user user --pass --file /path/to/file.txt")
-    fmt.Println("  snmp-switch-port-monitor --results")
+    fmt.Println("  snmp-switch-port-monitor --ip-file /file/ip-file.txt --out-file /file/result.dat")
     fmt.Println("  snmp-switch-port-monitor --help")
     os.Exit(0)
 }
 
+// read ip-file to get list of IP's
 func readFile(path string) ([]string, error) {
     file, err := os.Open(path)
     if err != nil {
@@ -63,7 +65,58 @@ func readFile(path string) ([]string, error) {
     return lines, scanner.Err()
 }
 
-func results() {
-    fmt.Println("Here will be results")
-    os.Exit(0)
+// write to file
+func writeFile(data string) {
+    if _, err := os.Stat(*outFileName) ; os.IsNotExist(err) {
+       file, err := os.OpenFile(*outFileName, os.O_CREATE, 0644)
+       if err != nil {
+           log.Fatalf("error: cant create file, %s", err)
+       }
+
+       file.Close()
+    }
+
+    file, err := os.OpenFile(*outFileName, os.O_WRONLY|os.O_APPEND, 0644)
+    if err != nil {
+        log.Fatalf("error: can't open file, %s", err)
+    }
+    defer file.Close()
+
+    length, err := file.WriteString(data)
+    if err != nil {
+        log.Fatalf("error: cant' write to file, %s%d", err, length)
+    }
+}
+
+// getting SNMP data from host
+func getSnmpData(host string) {
+    fmt.Printf("getting data from %s\n", host)
+
+    g.Default.Target = host
+
+    err := g.Default.Connect()
+    if err != nil {
+        fmt.Printf("error: connect() to host %s\n", host)
+        return
+    }
+    defer g.Default.Conn.Close()
+
+    oids := []string{}
+    for idx := 1 ; idx <= 52 ; idx++ {
+        oids = append(oids, strings.Join([]string{"1.3.6.1.2.1.2.2.1.8.", strconv.Itoa(idx)}, ""))
+    }
+
+    result, err2 := g.Default.Get(oids) // Get() accepts up to g.MAX_OIDS
+    if err2 != nil {
+        log.Fatalf("Get() err: %v", err2)
+    }
+
+    for i, variable := range result.Variables {
+        if variable.Value == 1 || variable.Value == 2 {
+            portNumber := i + 1
+            t := time.Now().Format("2006-01-02 15:04:05")
+            s := fmt.Sprintf("%s\t%d\t%d\t%s\n", host, portNumber, variable.Value, t)
+            writeFile(s)
+        }
+    }
 }
