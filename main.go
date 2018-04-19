3d12344dc51da919c841f9cbaaf89c65c7c285e7
Igor Chaban <lintuh@gmail.com>
2018-04-19 13:00:48 +0300

    Small changes

--- main.go
+++ main.go
@@ -22,13 +22,13 @@ func main() {
         printUsage()
     }
 
-    data, err := readFile(*ipFileName)
+    data, err := readInputFile(*ipFileName)
     if err != nil {
         log.Fatalf("error: %s", err.Error())
     }
 
     if len(data) == 0 {
-        log.Fatal("error: file is empty")
+        log.Fatal("error: outFile is empty")
     }
 
     for ip := range data {
@@ -36,7 +36,8 @@ func main() {
     }
 }
 
-func printUsage()  {
+// print usage
+func printUsage() {
     fmt.Println("snmp-swith-port-monitor -- monitor port up/down state via SNMP")
     fmt.Println("")
     fmt.Println("Usage:")
@@ -49,7 +50,7 @@ func printUsage()  {
 }
 
 // read ip-file to get list of IP's
-func readFile(path string) ([]string, error) {
+func readInputFile(path string) ([]string, error) {
     file, err := os.Open(path)
     if err != nil {
         return nil, err
@@ -66,29 +67,29 @@ func readFile(path string) ([]string, error) {
 }
 
 // write to file
-func writeFile(data string) {
-    if _, err := os.Stat(*outFileName) ; os.IsNotExist(err) {
-       file, err := os.OpenFile(*outFileName, os.O_CREATE, 0644)
-       if err != nil {
-           log.Fatalf("error: cant create file, %s", err)
-       }
+func writeOutFile(data string) {
+    if _, err := os.Stat(*outFileName); os.IsNotExist(err) {
+        outFile, err := os.OpenFile(*outFileName, os.O_CREATE, 0644)
+        if err != nil {
+            log.Fatalf("error: cant create outFile, %s", err)
+        }
 
-       file.Close()
+        outFile.Close()
     }
 
-    file, err := os.OpenFile(*outFileName, os.O_WRONLY|os.O_APPEND, 0644)
+    outFile, err := os.OpenFile(*outFileName, os.O_WRONLY|os.O_APPEND, 0644)
     if err != nil {
-        log.Fatalf("error: can't open file, %s", err)
+        log.Fatalf("error: can't open outFile, %s", err)
     }
-    defer file.Close()
+    defer outFile.Close()
 
-    length, err := file.WriteString(data)
+    length, err := outFile.WriteString(data)
     if err != nil {
         log.Fatalf("error: cant' write to file, %s%d", err, length)
     }
 }
 
-// getting SNMP data from host
+// getting SNMP data from host and write them into file
 func getSnmpData(host string) {
     fmt.Printf("getting data from %s\n", host)
 
@@ -102,21 +103,22 @@ func getSnmpData(host string) {
     defer g.Default.Conn.Close()
 
     oids := []string{}
-    for idx := 1 ; idx <= 52 ; idx++ {
+    for idx := 1; idx <= 52; idx++ {
         oids = append(oids, strings.Join([]string{"1.3.6.1.2.1.2.2.1.8.", strconv.Itoa(idx)}, ""))
     }
 
-    result, err2 := g.Default.Get(oids) // Get() accepts up to g.MAX_OIDS
+    result, err2 := g.Default.Get(oids)
     if err2 != nil {
-        log.Fatalf("Get() err: %v", err2)
-    }
+        log.Printf("error: %s %v", host, err2)
+    } else {
+        for i, variable := range result.Variables {
+            if variable.Value == 1 || variable.Value == 2 {
+                portNumber := i + 1
+                t := time.Now().Format("2006-01-02 15:04:05")
+                s := fmt.Sprintf("%s\t%d\t%d\t%s\n", host, portNumber, variable.Value, t)
 
-    for i, variable := range result.Variables {
-        if variable.Value == 1 || variable.Value == 2 {
-            portNumber := i + 1
-            t := time.Now().Format("2006-01-02 15:04:05")
-            s := fmt.Sprintf("%s\t%d\t%d\t%s\n", host, portNumber, variable.Value, t)
-            writeFile(s)
+                writeOutFile(s)
+            }
         }
     }
 }
